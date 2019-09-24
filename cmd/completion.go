3614a4d06a9feb979a01432fd32b8333d86fbff4
Stephen <stephen.siegert@rstudio.com>
2019-09-24 14:50:49 -0400

    add bash completion script generator command

---
+++ cmd/completion.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+
+	rootCmd.AddCommand(completionCmd)
+}
+
+// completionCmd represents the completion command
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generates bash completion scripts for drip",
+	Long: `To load completion run
+
+. <(drip completion)
+
+To configure your bash shell to load completions for each session add to your bashrc
+
+# ~/.bashrc or ~/.profile
+. <(drip completion)
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		rootCmd.GenBashCompletion(os.Stdout)
+	},
+}
